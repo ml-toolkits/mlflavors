c54adbf8b49488d13241f285e6813a3f88351a32
Benjamin Bluhm <benjaminbluhm@gmx.de>
2023-01-02 16:35:49 +0100

    Switch to Sphinx RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "mlflow-flavors"
-copyright = "2022, Benjamin Bluhm"
+copyright = "2022, Blue Pen Labs"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -171,7 +171,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
