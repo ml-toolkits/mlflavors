52198045f22e5cd70ebfb853d081add052af6821
Benjamin Bluhm <40917599+benjaminbluhm@users.noreply.github.com>
2023-03-08 09:59:50 +0000

    Fix module imports

--- mlflow_flavors/__init__.py
+++ mlflow_flavors/__init__.py
@@ -1,10 +1,11 @@
 __version__ = "0.0.1"
 
 try:
-    from mlflow_flavors import orbit, sktime  # noqa: F401
+    # from mlflow_flavors import orbit
+    from mlflow_flavors import sktime  # noqa: F401
 
-    _model_flavors_supported = [
-        "orbit",
+    __all__ = [
+        # "orbit",
         "sktime",
     ]
 except ImportError as e:  # noqa: F841
